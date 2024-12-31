e3b3bd0d31d315a51c7810601b4f694d348bee30
Ram Surath Kumar <rampugazh2050@gmail.com>
2025-01-01 01:52:31 +0530

    first commit

---
+++ models/FormOrFolder.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ResponseSchema = new mongoose.Schema({
+  submittedAt: { type: Date, default: Date.now },
+  response: { type: Array },
+});
+
+const FolderOrFormSchema = new mongoose.Schema(
+  {
+    isFolder: { type: Boolean, default: false, required: true },
+    name: { type: String, required: true },
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Space",
+      required: true,
+    },
+    children: [{ type: mongoose.Schema.Types.ObjectId, ref: "FolderOrForm" }],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    data: { type: Array },
+    viewed: { type: Number },
+    started: { type: Number },
+    responses: [ResponseSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("FolderOrForm", FolderOrFormSchema);
---
+++ routes/userRouter.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const registerUser = require("../controllers/user/registerUser");
+const validateUser = require("../middleware/user/validateUser");
+const loginUser = require("../controllers/user/loginUser");
+const validateToken = require("../middleware/validateToken");
+const getUserDetails = require("../controllers/user/getUserDetails");
+const validateUpdateInput = require("../middleware/user/validateUpdateInput");
+const updateUserDetails = require("../controllers/user/updateUserDetails");
+const generateInvite = require("../controllers/user/generateInvite");
+const ShareSpace = require("../controllers/user/shareSpace");
+
+router.post("/register", validateUser, registerUser);
+router.post("/login", validateUser, loginUser);
+router.get("/", validateToken, getUserDetails);
+router.put("/update", validateToken, validateUpdateInput, updateUserDetails);
+router.get("/generate-invite", validateToken, generateInvite);
+router.put("/sharespace", validateToken, ShareSpace);
+
+module.exports = router;
